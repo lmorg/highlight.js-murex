ddf7f0baa9ba072e404d3c00fca1f7c82ed4672c
Laurence Morgan <laurence.morgan@nearform.com>
2026-05-18 19:05:51 +0100

    incorrectly exported functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 "use strict";
 
-module.exports = {
-  murex: require("./languages/murex")
-};
+const murex = require("./languages/murex");
+
+// Default export is the language function so users can call:
+//   hljs.registerLanguage("murex", require("highlightjs-murex"))
+// Named export is also available:
+//   const { murex } = require("highlightjs-murex")
+module.exports = murex;
+module.exports.murex = murex;
